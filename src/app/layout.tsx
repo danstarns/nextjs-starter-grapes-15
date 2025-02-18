a1bd76309f743da6cf508018bda7eac1bffad3d9
danstarns <danielstarns@hotmail.com>
2025-02-18 12:11:02 +0000

    feat: nextjs 15

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-import { Layout } from '@/components'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Layout>{children}</Layout>
+        {children}
       </body>
     </html>
   )
