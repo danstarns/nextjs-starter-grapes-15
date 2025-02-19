2cd80f13059ce9d1067889a471be726e35551b5b
danstarns <danielstarns@hotmail.com>
2025-02-19 13:35:55 +0000

    feat: add studio editor link

---
+++ src/app/studio/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import StudioEditor from '@grapesjs/studio-sdk/react';
+import '@grapesjs/studio-sdk/style';
+
+export default function DefaultEditor() {
+  return (
+    <StudioEditor
+    options={{
+      project: {
+        type: 'web',
+        default: {
+          pages: [
+            { name: 'Home', component: '<h1>Home page</h1>' },
+            { name: 'About', component: '<h1>About page</h1>' },
+            { name: 'Contact', component: '<h1>Contact page</h1>' },
+          ]
+        },
+      }
+    }}
+  />
+  );
+}
